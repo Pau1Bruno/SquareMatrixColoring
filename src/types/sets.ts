d8d3da57655dcf2ae1bc85b4d980df1d04420aa5
Pau1Bruno <paulbruno@bk.ru>
2025-03-10 01:13:59 +0300

    feat: add types

---
+++ src/types/sets.ts
@@ -0,0 +1,20 @@
+export interface realmB {
+    B1: number[];
+    B2: number[];
+    omega: any;
+    dSet: dSet[];
+}
+
+export interface realmBArr {
+    realms: realmB[];
+    records: {
+        B1: number[];
+        B2: number[];
+    };
+}
+
+export interface dSet {
+    i: number;
+    j: number;
+    value: number[];
+}
