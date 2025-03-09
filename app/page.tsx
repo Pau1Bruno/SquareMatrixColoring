23c7a285026cf1c564e0aaeea66798950703ce0a
Pau1Bruno <paulbruno@bk.ru>
2025-03-10 01:11:39 +0300

    build: update assets and configs

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <div className="grow bg-background font-[family-name:var(--font-geist-sans)]">
-            <div></div>
-        </div>
-    );
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,12 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
     darkMode: ['class', '[data-theme="dark"]'],
     content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    safelist: [
+        { pattern: /grid-cols-\d+/ },      // чтобы работали динамические grid-cols-XX
+        { pattern: /w-\[\d+px\]/ },       // чтобы работали классы вида w-[50px], w-[100px] и т.д.
+        { pattern: /h-\[\d+px\]/ },
     ],
     theme: {
         extend: {
